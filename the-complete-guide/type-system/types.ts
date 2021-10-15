0d8ad095890b3be2b4714bf01771ff4bd496934d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-10-15 14:20:05 +0630

    #add: type-system

---
+++ the-complete-guide/type-system/types.ts
@@ -0,0 +1,18 @@
+const today = new Date();
+today.getFullDate(); // error
+
+const num = 2;
+num.toPrecison(); // errror
+
+const person = {
+    name: "Maii",
+};
+console.log(person.age); // error
+
+class Animal {
+    fly() {
+        console.log("Fly");
+    }
+}
+const bird = new Animal();
+bird.swim(); // error
