a07e1cf3704af85ed014820ca7e5c219f96f0c9d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-10-15 11:41:20 +0630

    #add: getting-started

---
+++ the-complete-guide/getting-started/index.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = "https://jsonplaceholder.typicode.com/todos/1";
+axios_1["default"].get(url).then(function (res) {
+    var todo = res.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log("Todo id: " + id + "\n    Todo title: " + title + "\n    Todo status: " + completed + "\n    ");
+});
