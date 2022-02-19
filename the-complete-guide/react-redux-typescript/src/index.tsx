3f2a985616040f668b9f1beb93396b7d1e058274
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-02-19 16:50:09 +0630

    add: react-redux-typescript functional

--- the-complete-guide/react-redux-typescript/src/index.tsx
+++ the-complete-guide/react-redux-typescript/src/index.tsx
@@ -1,36 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 interface IAppProps {
   color?: string;
 }
 
-interface IAppState {
-  counter: number;
-}
-class App extends React.Component<IAppProps, IAppState> {
-  constructor(props: IAppProps) {
-    super(props);
-    this.state = { counter: 0 };
-  }
+const App = (props: IAppProps): JSX.Element => {
+  const [counter, setCounter] = useState<number>(0);
 
-  onIncClick = () => {
-    this.setState({ counter: this.state.counter + 1 });
+  const handleIncrement = () => {
+    setCounter((state) => state + 1);
   };
 
-  onDecClick = () => {
-    this.setState({ counter: this.state.counter - 1 });
+  const handleDecrement = () => {
+    setCounter((state) => state - 1);
   };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncClick}>inc</button>
-        <p>{this.state.counter}</p>
-        <button onClick={this.onDecClick}>dec</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <button onClick={handleIncrement}>inc</button>
+      <div>{counter}</div>
+      <button onClick={handleDecrement}>dec</button>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
