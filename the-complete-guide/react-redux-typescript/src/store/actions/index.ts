35f76d963848bbd257c11981d585c7814d3a2066
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-02-20 17:27:50 +0630

    add: react-redux-typescript axios response interface

--- the-complete-guide/react-redux-typescript/src/store/actions/index.ts
+++ the-complete-guide/react-redux-typescript/src/store/actions/index.ts
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { Dispatch } from "redux";
 
+interface ITodo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 const url = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTodos = () => async (dispatch: Dispatch) => {
-  const response = await axios.get(url);
+  const response = await axios.get<ITodo[]>(url);
   console.log(response);
   dispatch({
     type: "FETCH_TODOS",
-    payload: response.data.data,
+    payload: response.data,
   });
 };
