44679b47dd9b172188f0ba4bc0818dc6c891367c
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-02-27 19:26:47 +0630

    add: react-redux-typescript delete action in reducer

--- the-complete-guide/react-redux-typescript/src/store/reducers/todos.ts
+++ the-complete-guide/react-redux-typescript/src/store/reducers/todos.ts
@@ -4,6 +4,8 @@ export const todosReducer = (state: ITodo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: ITodo) => todo.id !== action.payload);
     default:
       return state;
   }
