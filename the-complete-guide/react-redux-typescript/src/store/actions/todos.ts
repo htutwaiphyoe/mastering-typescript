3595aa806d2141c31ec4a5f2625b05bb5102568b
htutwaiphyoe <htutwaiphyoe@gmail.com>
2022-02-28 19:46:17 +0630

    add: react-redux-typescript removing console.log

--- the-complete-guide/react-redux-typescript/src/store/actions/todos.ts
+++ the-complete-guide/react-redux-typescript/src/store/actions/todos.ts
@@ -21,7 +21,7 @@ const url = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTodoList = () => async (dispatch: Dispatch) => {
   const response = await axios.get<ITodo[]>(url);
-  console.log(response);
+
   dispatch<IFetchTodoList>({
     type: ActionTypes.fetchTodos,
     payload: response.data,
