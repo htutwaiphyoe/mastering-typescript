b929cb0d10012e14ae5332dcd5639220a25ce9df
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-01-14 10:41:56 +0630

    feat(typescript-fundamentals-v4): 12. Union Types

--- frontend-masters/typescript-fundamental-v4/lessions/src/06-union-and-intersection-types.ts
+++ frontend-masters/typescript-fundamental-v4/lessions/src/06-union-and-intersection-types.ts
@@ -1,22 +1,22 @@
 //* Union types in TypeScript
 
-const humidity = 79 //? Recall literal types
+const humidity = 79; //? Recall literal types
 
 //? Create types for two sets of numbers
 
 //? A set of numbers from 1 to 5
-type OneThroughFive = 1 | 2 | 3 | 4 | 5
-let lowNumber: OneThroughFive = 3 //✔️ Valid
-// lowNumber = 8 //! 8 is not in the set
+type OneThroughFive = 1 | 2 | 3 | 4 | 5;
+let lowNumber: OneThroughFive = 3; //✔️ Valid
+lowNumber = 8; //! 8 is not in the set
 
 //? A set of even numbers from 1 to 9
-type Evens = 2 | 4 | 6 | 8
-let evenNumber: Evens = 2 //✔️ Valid
-// evenNumber = 5; //! 5 is not in the set
+type Evens = 2 | 4 | 6 | 8;
+let evenNumber: Evens = 2; //✔️ Valid
+evenNumber = 5; //! 5 is not in the set
 
-/*
 // //? A set of numbers from 1 to 5 OR a set of even numbers from 1 to 9
-// let evenOrLowNumber = 5 as Evens | OneThroughFive;
+let evenOrLowNumber: Evens | OneThroughFive = 5;
+evenOrLowNumber = 4;
 
 /*
 // //? Control flow sometimes results in union types
@@ -107,4 +107,4 @@ let evenNumber: Evens = 2 //✔️ Valid
 
 /**/
 
-export default {}
+export default {};
