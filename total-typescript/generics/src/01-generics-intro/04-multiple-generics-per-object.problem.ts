5aa40019b84734cc0a719e11b1ad9b7240dd2926
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-15 02:20:18 +0700

    feat(total-typescript: generic): 04-multiple-generics-per-object

--- total-typescript/generics/src/01-generics-intro/04-multiple-generics-per-object.problem.ts
+++ total-typescript/generics/src/01-generics-intro/04-multiple-generics-per-object.problem.ts
@@ -1,7 +1,12 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = (params: { a: unknown; b: unknown }) => {
+type Params<A, B> = {
+  a: A;
+  b: B;
+};
+
+const returnBothOfWhatIPassIn = <A, B>(params: Params<A, B>) => {
   return {
     first: params.a,
     second: params.b,
