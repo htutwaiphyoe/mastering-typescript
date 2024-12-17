e450bfa6ab76a47d78d96dea8460fb864ab9dabb
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-17 23:00:50 +0700

    feat(total-typescript: generics): 13-typed-object-keys

--- total-typescript/generics/src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
+++ total-typescript/generics/src/03-art-of-type-arguments/13-typed-object-keys.problem.ts
@@ -6,8 +6,8 @@ import { Equal, Expect } from "../helpers/type-utils";
  * to do with the way you specify the generic. Can you get
  * both solutions?
  */
-const typedObjectKeys = (obj: unknown) => {
-  return Object.keys(obj);
+const typedObjectKeys = <T extends string>(params: Record<T, number>): T[] => {
+  return Object.keys(params) as T[];
 };
 
 it("Should return the keys of the object", () => {
