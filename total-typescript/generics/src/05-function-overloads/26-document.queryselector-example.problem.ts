9dcf8b4b12ae748b9217351f1aab8adf90d208d2
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-19 00:48:41 +0700

    feat(total-typescript: generics): 26-document.querySelector

--- total-typescript/generics/src/05-function-overloads/26-document.queryselector-example.problem.ts
+++ total-typescript/generics/src/05-function-overloads/26-document.queryselector-example.problem.ts
@@ -7,10 +7,10 @@ const spanElement = document.querySelector("span");
  * Your challenge: figure out why divElement2 is NOT
  * of type HTMLDivElement.
  */
-const divElement2 = document.querySelector("div.foo");
+const divElement2 = document.querySelector<HTMLDivElement>("div.foo");
 
 type tests = [
   Expect<Equal<typeof divElement, HTMLDivElement | null>>,
   Expect<Equal<typeof spanElement, HTMLSpanElement | null>>,
-  Expect<Equal<typeof divElement2, HTMLDivElement | null>>
+  Expect<Equal<typeof divElement2, HTMLDivElement | null>>,
 ];
