770932459c84fa5930639edcc95310d452e50cdb
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-19 01:13:54 +0700

    feat(total-typescript: generics): 28-could-be-instantiated-with-subtype=of

--- total-typescript/generics/src/05-function-overloads/28-could-be-instantiated-with-subtype-of.problem.ts
+++ total-typescript/generics/src/05-function-overloads/28-could-be-instantiated-with-subtype-of.problem.ts
@@ -8,9 +8,11 @@ const obj = {
 
 type ObjKey = keyof typeof obj;
 
-const getObjValue = <TKey extends ObjKey>(key: TKey = "a") => {
+function getObjValue(): (typeof obj)["a"];
+function getObjValue<TKey extends ObjKey>(key: TKey): (typeof obj)[TKey];
+function getObjValue(key: ObjKey = "a") {
   return obj[key];
-};
+}
 
 const one = getObjValue("a");
 const oneByDefault = getObjValue();
@@ -21,5 +23,5 @@ type tests = [
   Expect<Equal<typeof one, 1>>,
   Expect<Equal<typeof oneByDefault, 1>>,
   Expect<Equal<typeof two, 2>>,
-  Expect<Equal<typeof three, 3>>
+  Expect<Equal<typeof three, 3>>,
 ];
