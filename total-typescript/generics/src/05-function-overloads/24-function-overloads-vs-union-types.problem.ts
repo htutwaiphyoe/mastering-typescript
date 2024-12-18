6c1c832b0a7955452a8139540b99f1fcb5d362bb
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-19 00:23:43 +0700

    feat(total-typescript: generics): 24-function-overload-vs-union-types

--- total-typescript/generics/src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
+++ total-typescript/generics/src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function runGenerator(generator: unknown) {
+function runGenerator(generator: (() => string) | { run: () => string }) {
   if (typeof generator === "function") {
     return generator();
   }
