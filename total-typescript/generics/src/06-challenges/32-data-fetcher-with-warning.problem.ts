b6bd8e82261aa8e00de6ee3c080b575bc9c21d98
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-20 23:55:56 +0700

    feat(total-typescript: generics): 32-data-fetcher-with-warning

--- total-typescript/generics/src/06-challenges/32-data-fetcher-with-warning.problem.ts
+++ total-typescript/generics/src/06-challenges/32-data-fetcher-with-warning.problem.ts
@@ -1,14 +1,18 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fetchData = async <TResult>(url: string): Promise<TResult> => {
+const fetchData = async <
+  TResult = "You must pass a type argument to fetchData",
+>(
+  url: string
+): Promise<TResult> => {
   const data = await fetch(url).then((response) => response.json());
   return data;
 };
 
 it("Should fetch data from an API", async () => {
   const data = await fetchData<{ name: string }>(
-    "https://swapi.dev/api/people/1",
+    "https://swapi.dev/api/people/1"
   );
   expect(data.name).toEqual("Luke Skywalker");
 
