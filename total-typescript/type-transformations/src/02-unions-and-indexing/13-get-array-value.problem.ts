041c1dd46863e93677f60c217595b75425df485f
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-22 00:21:32 +0700

    feat(total-typescript: type-transformations): 13-get-array=value

--- total-typescript/type-transformations/src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ total-typescript/type-transformations/src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -1,9 +1,9 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fruits = ["apple", "banana", "orange"];
+const fruits = ["apple", "banana", "orange"] as const;
 
-type AppleOrBanana = unknown;
-type Fruit = unknown;
+type Fruit = (typeof fruits)[number];
+type AppleOrBanana = Extract<Fruit, "apple" | "banana">;
 
 type tests = [
   Expect<Equal<AppleOrBanana, "apple" | "banana">>,
