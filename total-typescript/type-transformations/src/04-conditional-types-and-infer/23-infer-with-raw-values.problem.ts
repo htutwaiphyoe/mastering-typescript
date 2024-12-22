192876856277ee04666a12fe820205c493851e94
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-23 01:47:18 +0700

    feat(total-typescript: type-transformations): 23.infer-with-raw-values

--- total-typescript/type-transformations/src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
+++ total-typescript/type-transformations/src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
@@ -1,6 +1,6 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type GetDataValue<T> = unknown;
+type GetDataValue<T> = T extends Record<"data", infer V> ? V : never;
 
 type tests = [
   Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
