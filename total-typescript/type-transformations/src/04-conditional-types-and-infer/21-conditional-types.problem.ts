4dc4e8193cd55a8a571cde8055bdbf9f4338868a
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-23 01:01:56 +0700

    feat(total-typescript: type-transformations): 21.conditional-types

--- total-typescript/type-transformations/src/04-conditional-types-and-infer/21-conditional-types.problem.ts
+++ total-typescript/type-transformations/src/04-conditional-types-and-infer/21-conditional-types.problem.ts
@@ -1,6 +1,6 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello = unknown;
+type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
