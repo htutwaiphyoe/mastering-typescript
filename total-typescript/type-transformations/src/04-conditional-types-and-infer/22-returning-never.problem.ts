38902fe7fcaf35ab148398c2a42a81b16df6c5bb
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-23 01:17:03 +0700

    feat(total-typescript: type-transformations): 22.-returning-never

--- total-typescript/type-transformations/src/04-conditional-types-and-infer/22-returning-never.problem.ts
+++ total-typescript/type-transformations/src/04-conditional-types-and-infer/22-returning-never.problem.ts
@@ -1,6 +1,10 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
+type YouSayGoodbyeAndISayHello<T> = T extends "hello" | "goodbye"
+  ? T extends "hello"
+    ? "goodbye"
+    : "hello"
+  : never;
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
