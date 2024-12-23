c838acdfd2682e7dbcb4b7979278942ac733256c
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-23 21:13:22 +0700

    feat(total-typescript: type-transformations): 25-template-literal-value-extraction

--- total-typescript/type-transformations/src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
+++ total-typescript/type-transformations/src/04-conditional-types-and-infer/25-template-literal-value-extraction.problem.ts
@@ -8,7 +8,7 @@ type Names = [
   "BB King",
 ];
 
-type GetSurname<T> = unknown;
+type GetSurname<T> = T extends `${string} ${infer S}` ? S : never;
 
 type tests = [
   Expect<Equal<GetSurname<Names[0]>, "Pocock">>,
