c01e53e082b7e2049c4a7b805ad3af5683f7e355
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-27 02:21:09 +0700

    feat(total-typescript: type-transformations): 39-discriminated-union-with-unique-values-to-objects

--- total-typescript/type-transformations/src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
+++ total-typescript/type-transformations/src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
@@ -12,7 +12,9 @@ type Route =
   | { route: "/admin" }
   | { route: "/admin/users" };
 
-type RoutesObject = unknown;
+type RoutesObject = {
+  [R in Route as R["route"]]: R extends { search: infer S } ? S : never;
+};
 
 type tests = [
   Expect<
