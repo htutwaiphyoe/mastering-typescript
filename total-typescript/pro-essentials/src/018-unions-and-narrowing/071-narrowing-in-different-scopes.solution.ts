c23faf8bf281add6bcdb185e57947c1b6cb974da
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-28 21:07:43 +0700

    feat(total-typescript: pro-essentials): project setup

---
+++ total-typescript/pro-essentials/src/018-unions-and-narrowing/071-narrowing-in-different-scopes.solution.ts
@@ -0,0 +1,47 @@
+import { Equal, Expect } from "@total-typescript/helpers";
+import { expect, it } from "vitest";
+
+const findUsersByName = (
+  searchParams: { name?: string },
+  users: {
+    id: string;
+    name: string;
+  }[],
+) => {
+  const searchParamsName = searchParams.name;
+  // let searchParamsName = searchParams.name; // Also works with let!
+  if (searchParamsName) {
+    return users.filter((user) => {
+      return user.name.includes(searchParamsName);
+    });
+  }
+
+  return users;
+};
+
+it("Should find the exact name", () => {
+  const result = findUsersByName(
+    {
+      name: "Bob",
+    },
+    [
+      {
+        id: "1",
+        name: "Bob",
+      },
+      {
+        id: "2",
+        name: "Alice",
+      },
+    ],
+  );
+
+  expect(result).toEqual([
+    {
+      id: "1",
+      name: "Bob",
+    },
+  ]);
+
+  type test = Expect<Equal<typeof result, { id: string; name: string }[]>>;
+});
