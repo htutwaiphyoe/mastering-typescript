28433c8da5f0e5d232b6fe45a6ea1c180411712a
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-29 22:16:48 +0700

    feat(total-typescript: advanced-patterns): 07-ad-function-to-global-scope

--- total-typescript/advanced-patterns/src/02-globals/07-add-function-to-global-scope.problem.ts
+++ total-typescript/advanced-patterns/src/02-globals/07-add-function-to-global-scope.problem.ts
@@ -1,21 +1,10 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-/**
- * Clues:
- *
- * 1. declare global will be needed:
- *
- * declare global {}
- *
- * 2. myFunc will need to be added to the global scope using 'function':
- *
- * function myFunc(): boolean
- *
- * 3. myVar will need to be added to the global scope using 'var':
- *
- * var myVar: number
- */
+declare global {
+  function myFunc(): boolean;
+  var myVar: number;
+}
 
 globalThis.myFunc = () => true;
 globalThis.myVar = 1;
