0877b37aaf7b85fb252744408b96d679a08c5b95
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-31 01:11:42 +0700

    feat(total-typescript: advanced-patterns): 12-type-predicates-with-filter

--- total-typescript/advanced-patterns/src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.problem.ts
+++ total-typescript/advanced-patterns/src/03-type-predicates-assertion-functions/12-type-predicates-with-filter.problem.ts
@@ -3,7 +3,9 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 export const values = ["a", "b", undefined, "c", undefined];
 
-const filteredValues = values.filter((value) => Boolean(value));
+const filteredValues = values.filter((value): value is string =>
+  Boolean(value)
+);
 
 it("Should filter out the undefined values", () => {
   expect(filteredValues).toEqual(["a", "b", "c"]);
