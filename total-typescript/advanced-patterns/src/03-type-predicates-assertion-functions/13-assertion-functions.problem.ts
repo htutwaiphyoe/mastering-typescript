0a863e32db1a188a711b8341f3316501d988507b
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2024-12-31 01:27:46 +0700

    feat(total-typescript: advanced-patterns): 13-assertion-functions

--- total-typescript/advanced-patterns/src/03-type-predicates-assertion-functions/13-assertion-functions.problem.ts
+++ total-typescript/advanced-patterns/src/03-type-predicates-assertion-functions/13-assertion-functions.problem.ts
@@ -15,7 +15,9 @@ interface NormalUser extends User {
   role: "normal";
 }
 
-function assertUserIsAdmin(user: NormalUser | AdminUser) {
+function assertUserIsAdmin(
+  user: NormalUser | AdminUser
+): asserts user is AdminUser {
   if (user.role !== "admin") {
     throw new Error("Not an admin user");
   }
