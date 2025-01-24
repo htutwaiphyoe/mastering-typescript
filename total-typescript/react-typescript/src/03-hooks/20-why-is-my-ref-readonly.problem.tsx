597efc156e5f79a1064a72d3b6e095a9477bbef4
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-01-25 02:00:10 +0700

    feat(react-typescript): 20-why-is-my-ref-readonly

--- total-typescript/react-typescript/src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
+++ total-typescript/react-typescript/src/03-hooks/20-why-is-my-ref-readonly.problem.tsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 
 export const Component = () => {
-  const ref = useRef<string>(null);
+  const ref = useRef<string | null>(null);
 
   // Why is this not allowed?
   ref.current = "Hello";
