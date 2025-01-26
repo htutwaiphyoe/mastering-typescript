0b520c1e2d33ece7e5f333d53676bf18e00f2f4c
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-01-26 15:16:49 +0700

    feat(react-typescript): 23-destructuring-discriminated-unions

--- total-typescript/react-typescript/src/04-advanced-props/23-destructuring-discriminated-unions.problem.tsx
+++ total-typescript/react-typescript/src/04-advanced-props/23-destructuring-discriminated-unions.problem.tsx
@@ -16,10 +16,11 @@ type ModalProps =
       title: string;
     };
 
-export const Modal = ({ variant, title }: ModalProps) => {
-  if (variant === "no-title") {
+export const Modal = (props: ModalProps) => {
+  if (props.variant === "no-title") {
     return <div>No title</div>;
   } else {
+    const { title } = props;
     return <div>Title: {title}</div>;
   }
 };
