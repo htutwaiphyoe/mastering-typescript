899e2a1661a1853078a0b8fa77b96376ea482ea6
hwp <70026748+htutwaiphyoe@users.noreply.github.com>
2025-01-26 16:32:26 +0700

    feat(react-typescript): 31-as-const

--- total-typescript/react-typescript/src/04-advanced-props/31-as-const.problem.ts
+++ total-typescript/react-typescript/src/04-advanced-props/31-as-const.problem.ts
@@ -4,10 +4,10 @@ const BACKEND_TO_FRONTEND_STATUS_MAP = {
   0: "pending",
   1: "success",
   2: "error",
-};
+} as const;
 
-type BackendStatus = unknown;
-type FrontendStatus = unknown;
+type BackendStatus = keyof typeof BACKEND_TO_FRONTEND_STATUS_MAP;
+type FrontendStatus = (typeof BACKEND_TO_FRONTEND_STATUS_MAP)[BackendStatus];
 
 type test = [
   Expect<Equal<BackendStatus, 0 | 1 | 2>>,
